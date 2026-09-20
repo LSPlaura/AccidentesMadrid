ebea0d6c8e1ddc3220c385af6b20a1253530d638
Laura <lsp3114@gmail.com>
2026-09-20 18:21:51 +0200

    fix(infrastructure): fix CSV paths to use AccidentesFolder

--- Config/Config.cs
+++ Config/Config.cs
@@ -5,7 +5,7 @@ public static class Config
     public static readonly string BaseDirectory = AppContext.BaseDirectory;
     public static readonly string CsvsFolder = Path.Combine(BaseDirectory, "Data");
     public static readonly string AccidentesFolder = Path.Combine(CsvsFolder,"Accidentes");
-    public static readonly string Csv2024Path = Path.Combine(CsvsFolder, "2024-accidentes-trafico-detalle.csv");
-    public static readonly string Csv2025Path = Path.Combine(CsvsFolder, "2025-accidentes-trafico-detalle-csv.csv");
-    public static readonly string Csv2026Path = Path.Combine(CsvsFolder, "2026-accidentes-trafico-detalle-csv.csv");
+    public static readonly string Csv2024Path = Path.Combine(AccidentesFolder, "2024-accidentes-trafico-detalle.csv");
+    public static readonly string Csv2025Path = Path.Combine(AccidentesFolder, "2025-accidentes-trafico-detalle-csv.csv");
+    public static readonly string Csv2026Path = Path.Combine(AccidentesFolder, "2026-accidentes-trafico-detalle-csv.csv");
 }
